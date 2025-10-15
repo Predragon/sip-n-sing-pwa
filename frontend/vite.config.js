e4f3093eee549fc8e468b05ace7db0b92a4914ee
Predrag Mirkovic <pmirkovic@yahoo.com>
2025-10-15 09:36:19 +0800

    Auto commit: 2025-10-15 09:36:19

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,17 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  root: './',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  },
   plugins: [
+    react(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['*.png', '*.svg', '*.ico'],
       manifest: {
         name: 'Sip & Sing Restobar',
         short_name: 'Sip & Sing',
-        description: 'Order delicious food and drinks from Sip & Sing Restobar',
+        description: 'Order delicious food and drinks',
         theme_color: '#4B0082',
         background_color: '#1a0033',
         display: 'standalone',
@@ -34,22 +26,6 @@ export default defineConfig({
             src: 'icon-512.png',
             sizes: '512x512',
             type: 'image/png'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/cdnjs\.cloudflare\.com\/.*/i,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'cdn-cache',
-              expiration: {
-                maxEntries: 10,
-                maxAgeSeconds: 60 * 60 * 24 * 365
-              }
-            }
           }
         ]
       }
